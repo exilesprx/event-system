21fe44294fd643f8ad04e0cf1c45c9f602e52223
Exilesprx <campbell.andrew86@yahoo.com>
2019-04-29 22:29:05 -0400

    Minor refactor
    Refactoring a few amqp functions and moving amqp stuff into the amqp
    package.

--- amqp/rabbit.go
+++ amqp/rabbit.go
@@ -2,13 +2,27 @@ package amqp
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/streadway/amqp"
 
 	"github.com/exilesprx/event-system/log"
 )
 
-func Connect(user string, password, host string, port int) *amqp.Connection {
+func Connect() *amqp.Connection {
+	user := os.Getenv("AMQP_USER")
+
+	password := os.Getenv("AMQP_PASSWORD")
+
+	host := os.Getenv("AMQP_HOST")
+
+	port, _ := strconv.Atoi(os.Getenv("AMQP_PORT"))
+
+	return connect(user, password, host, port)
+}
+
+func connect(user string, password, host string, port int) *amqp.Connection {
 	url := fmt.Sprintf("amqp://%s:%s@%s:%d", user, password, host, port)
 
 	conn, err := amqp.Dial(url)
@@ -20,7 +34,7 @@ func Connect(user string, password, host string, port int) *amqp.Connection {
 	return conn
 }
 
-func Channel(connection amqp.Connection) {
+func channel(connection amqp.Connection) {
 	channel, err := connection.Channel()
 
 	log.FailOnError(err, "Failed to create channel")
--- main.go
+++ main.go
@@ -3,10 +3,6 @@ package main
 import (
 	"github.com/exilesprx/event-system/amqp"
 
-	"strconv"
-
-	"os"
-
 	"github.com/exilesprx/event-system/log"
 
 	"github.com/joho/godotenv"
@@ -15,15 +11,7 @@ import (
 func main() {
 	loadEnv()
 
-	user := os.Getenv("AMQP_USER")
-
-	password := os.Getenv("AMQP_PASSWORD")
-
-	host := os.Getenv("AMQP_HOST")
-
-	port, _ := strconv.Atoi(os.Getenv("AMQP_PORT"))
-
-	amqp.Connect(user, password, host, port)
+	amqp.Connect()
 }
 
 func loadEnv() {
