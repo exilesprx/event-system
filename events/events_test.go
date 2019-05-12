e31b1683e1145ae35be3d2dd4452fa9804c685de
Exilesprx <campbell.andrew86@yahoo.com>
2019-05-11 22:19:51 -0400

    Event handler test update
    Updating the events handler test to allow for multiple test runs with
    different event counts.

--- events/events_test.go
+++ events/events_test.go
@@ -1,10 +1,12 @@
 package events
 
 import (
+	"fmt"
 	"testing"
 )
 
 type TestHandler struct {
+	EventName string
 }
 
 func (handler *TestHandler) Handle(message string) {
@@ -12,18 +14,32 @@ func (handler *TestHandler) Handle(message string) {
 }
 
 func (handler *TestHandler) GetEventName() string {
-	return "test"
+	return handler.EventName
 }
 
 func TestRegisterHandler(t *testing.T) {
-	events := New()
-	handler := &TestHandler{}
+	t.Run("1", testRegisterHandler(1))
 
-	events.RegisterHandler(handler)
+	t.Run("5", testRegisterHandler(5))
 
-	allEvents := events.GetEventHandlers()
+	t.Run("20", testRegisterHandler(20))
+}
 
-	if len(allEvents) != 1 {
-		t.Fail()
+func testRegisterHandler(eventCount int) func(t *testing.T) {
+	return func(t *testing.T) {
+		events := New()
+		handler := &TestHandler{}
+
+		for i := 0; i < eventCount; i++ {
+			handler.EventName = fmt.Sprintf("test%d", i)
+
+			events.RegisterHandler(handler)
+		}
+
+		allEvents := events.GetEventHandlers()
+
+		if len(allEvents) != eventCount {
+			t.Fail()
+		}
 	}
 }
